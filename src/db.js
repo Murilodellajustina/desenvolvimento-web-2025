e94b27f3e6914b80db9d7ad06e615a9a7d5c887d
Murilo Della Justina <161324281+Murilodellajustina@users.noreply.github.com>
2025-12-09 11:51:41 -0300

    Alterando db.js

--- src/db.js
+++ src/db.js
@@ -6,7 +6,7 @@ const isProduction = process.env.NODE_ENV === "production";
 const HOST = process.env.PGHOST || process.env.DB_HOST || "dpg-d4qt9cu3jp1c739nre2g-a.virginia-postgres.render.com";
 const PORT = process.env.PGPORT || process.env.DB_PORT || "5432"; 
 const DATABASE = process.env.PGDATABASE || process.env.DB_DATABASE || "db_agendamentos_4ybg";
-const USER = process.env.PGUSER || process.env.DB_USER || "postgres";
+const USER = process.env.PGUSER || process.env.DB_USER || "murilo_djota";
 const PASSWORD = process.env.PGPASSWORD || process.env.DB_PASSWORD || "3vRWKIe9An4qbBbtne9H9irdeSzLh2ZB";
 const pool = new Pool({
   host: HOST,         
